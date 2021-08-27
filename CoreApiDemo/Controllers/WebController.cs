08685e268f5b6533caaa634c6b3c60c0d9c06835
KothaiNagaraj1009 <87559312+KothaiNagaraj1009@users.noreply.github.com>
2021-08-27 00:55:53 -0700

    Add files via upload

---
+++ CoreApiDemo/Controllers/WebController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreApiDemo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class WebController : ControllerBase
+    {
+        private static List<string> studentsname = new List<string> { "Abu", "Little", "John", "Tommy" };
+
+       
+        public IEnumerable<string> GetNames()
+        {
+            return studentsname;
+        }
+       
+    }
+}
